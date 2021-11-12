ac0bec2a783d786df53fca78c1d3af756b59ee89
Tomer27 <tomeer.sch27@gmail.com>
2021-11-12 17:20:00 +0200

    stock's 'historical quotes'; search range of dates; print dates

--- src/selenium/stockInfo.java
+++ src/selenium/stockInfo.java
@@ -1,10 +1,20 @@
 package selenium;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.List;
 
+import org.apache.poi.EncryptedDocumentException;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.ss.usermodel.WorkbookFactory;
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -14,7 +24,14 @@ public class stockInfo {
 	static WebDriver driver;
 	
 	// symbol of the stock you want to search (in CAPITAL LETTERS)
-	static String stockSymbol = "AMZN";
+	static String stockSymbol = "TSLA";
+	
+	static String histQte = "Historical Quotes";
+	
+	// start & end dates - format of MM/DD/YYYY
+	static String startDate = "01/27/2019";
+	static String endDate = "01/27/2020";
+	
 	
 	// 'search' button in the main page
 	static public WebElement mainSearchBtn(WebDriver driver) {
@@ -64,6 +81,95 @@ public class stockInfo {
 		return currentPrice;
 	}
 	
+	// stock's options bar
+	static public List<WebElement> optionBar(WebDriver driver) {
+		List<WebElement> optionBar = driver.findElements(By.xpath("//*[@id=\"maincontent\"]/div[5]/div/div/li/a"));
+		return optionBar;
+	}
+	
+	// stock's historical quotes
+	static public WebElement historyQ(WebDriver driver) {
+		WebElement historyQ = driver.findElement(By.xpath("//*[@id=\"maincontent\"]/div[5]/div/div/li[5]/a"));
+		return historyQ;
+	}
+	
+	// 'start date' field
+	static public WebElement startDateField(WebDriver driver) {
+		WebElement startDateField = driver.findElement(By.xpath("//*[@id=\"maincontent\"]/div[6]/div/mw-downloaddata/form/div/div[2]/div[1]/input"));
+		return startDateField;
+	}
+	
+	// 'start date' field - 'clear' button
+	static public WebElement clearStartBtn(WebDriver driver) {
+		WebElement clearStartBtn = driver.findElement(By.xpath("//*[@id=\"maincontent\"]/div[6]/div/mw-downloaddata/form/div/div[2]/div[1]/div/div/div/footer/a[2]"));
+		return clearStartBtn;
+	}
+	
+	// 'start date' field - 'close' button
+	static public WebElement closeStartBtn(WebDriver driver) {
+		WebElement closeStartBtn = driver.findElement(By.xpath("//*[@id=\"maincontent\"]/div[6]/div/mw-downloaddata/form/div/div[2]/div[1]/div/div/div/footer/a[3]"));
+		return closeStartBtn;
+	}
+	
+	// 'end date' field
+	static public WebElement endDateField(WebDriver driver) {
+		WebElement endDateField = driver.findElement(By.xpath("//*[@id=\"maincontent\"]/div[6]/div/mw-downloaddata/form/div/div[2]/div[2]/input"));
+		return endDateField;
+	}
+	
+	// 'end date' field - 'clear' button
+	static public WebElement clearEndBtn(WebDriver driver) {
+		WebElement clearEndBtn = driver.findElement(By.xpath("//*[@id=\"maincontent\"]/div[6]/div/mw-downloaddata/form/div/div[2]/div[2]/div/div/div/footer/a[2]"));
+		return clearEndBtn;
+	}
+	
+	// 'end date' field - 'close' button
+	static public WebElement closeEndBtn(WebDriver driver) {
+		WebElement closeEndBtn = driver.findElement(By.xpath("//*[@id=\"maincontent\"]/div[6]/div/mw-downloaddata/form/div/div[2]/div[2]/div/div/div/footer/a[3]"));
+		return closeEndBtn;
+	}
+		
+	// 'update results' button
+	static public WebElement upResBtn(WebDriver driver) {
+		WebElement upResBtn = driver.findElement(By.xpath("//*[@id=\"maincontent\"]/div[6]/div/mw-downloaddata/form/div/button"));
+		return upResBtn;
+	}
+	
+	// write to excel file function
+	public static void excelPrint(String filePath, String value, int rowNumber, int cellNumber) throws EncryptedDocumentException, IOException {
+		FileInputStream inputStream = new FileInputStream(new File(filePath));
+		Workbook workBook = WorkbookFactory.create(inputStream);
+		Sheet sheet = workBook.getSheetAt(0);
+		Row r = sheet.getRow(rowNumber);
+		if (r == null) {
+			r = sheet.createRow(rowNumber);
+		}
+		org.apache.poi.ss.usermodel.Cell c = r.getCell(cellNumber);
+		if (c == null) {
+			c = r.createCell(cellNumber);
+		}
+		c.setCellValue(value);
+		FileOutputStream outputStream = new FileOutputStream(filePath);
+		workBook.write(outputStream);
+		workBook.close();
+		outputStream.close();
+	}
+	
+	// stock's page >> historical quotes >> dates
+	static public List<WebElement> tableDates(WebDriver driver) {
+		List<WebElement> tableDates = driver.findElements(By.xpath("//*[@id=\"download-data-tabs\"]/div/div[1]/table/tbody/child::tr/td[1]/div[1]"));
+		return tableDates;
+	}
+	
+	
+	
+	
+	// stock's page >> historical quotes >> next page button
+	static public WebElement nextPageBtn(WebDriver driver) {
+		WebElement nextPageBtn = driver.findElement(By.xpath("//*[@id=\"download-data-tabs\"]/div/div[2]/a[2]"));
+		return nextPageBtn;
+	}
+	
 	
 	public static void main(String[] args) throws InterruptedException {
 		// enter path to 'chromedriver.exe' on your PC
@@ -100,12 +206,63 @@ public class stockInfo {
 		
 		// print to console: yields by durations
 		System.out.println("It's yields are:");
-		Thread.sleep(1500);
+		Thread.sleep(500);
 		for (int i = 0; i < durations(driver).size(); i++) {
 			String durX = durations(driver).get(i).getText();
 			String yldX = yields(driver).get(i).getText();
 			System.out.println(durX + ": " + yldX);
-		}	
+		}
+		System.out.println("------------------");
+		Thread.sleep(1500);
+		
+		// go to stock's 'historical quotes'
+		JavascriptExecutor js = (JavascriptExecutor)driver;
+		try {			
+			js.executeScript("window.scrollBy(0,650)", "");
+			Thread.sleep(1500);
+			historyQ(driver).click();
+			System.out.println("History qoute clicked.");
+			Thread.sleep(2000);
+		} catch (Exception e) {
+			System.err.println("History qoute not clicked.");
+		}
+				
+		// search by range of dates
+		js.executeScript("window.scrollBy(0,750)", "");
+		try {			
+			startDateField(driver).click();
+			Thread.sleep(500);
+			clearStartBtn(driver).click();
+			startDateField(driver).click();
+			startDateField(driver).sendKeys(startDate);
+			System.out.println("Set startDate.");
+		} catch (Exception e) {
+			System.err.println("Didn't set startDate.");
+		}		
+		try {
+			endDateField(driver).click();
+			Thread.sleep(500);
+			clearEndBtn(driver).click();
+			endDateField(driver).click();
+			endDateField(driver).sendKeys(endDate);
+			System.out.println("Set endDate.");
+		} catch (Exception e) {
+			System.err.println("Didn't set endDate.");
+		}		
+		upResBtn(driver).click();
+		Thread.sleep(3000);
+		
+		// print the dates out of the historical dates table
+		try {
+			System.out.println(tableDates(driver).size());
+		} catch (Exception e) {
+			System.err.println("Couldn't print the dates out of the historical dates table");
+		}
+		
+		
+		
+		
+		
 		
 		driver.quit();
 	}
